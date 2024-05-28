736d44c308cd0c4d7ae68edf5a7a5d62a7d3c668
Laurareis1 <laura.psdreis@gmail.com>
2024-05-28 15:57:18 -0100

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from forms import RegistrationForm, LoginForm, TaskForm
+from models import db, User, Task
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/tasks', methods=['GET', 'POST'])
+@login_required
+def tasks():
+    form = TaskForm()
+    if form.validate_on_submit():
+        task = Task(description=form.description.data, author=current_user)
+        db.session.add(task)
+        db.session.commit()
+        flash('Your task has been created!', 'success')
+        return redirect(url_for('tasks'))
+    tasks = Task.query.filter_by(author=current_user)
+    return render_template('tasks.html', form=form, tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
